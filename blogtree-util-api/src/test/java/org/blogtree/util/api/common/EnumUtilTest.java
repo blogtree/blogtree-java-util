156ef683cfeba06f9a1596f9b604e0cf1ea44896
AlanGeeker <alangeeker@outlook.com>
2021-10-22 07:25:46 +0800

    EnumUtilTest

--- blogtree-util-api/src/test/java/org/blogtree/util/api/common/EnumUtilTest.java
+++ blogtree-util-api/src/test/java/org/blogtree/util/api/common/EnumUtilTest.java
@@ -1,14 +1,20 @@
 package org.blogtree.util.api.common;
 
+import org.blogtree.util.api.base.enums.UserTypeEnum;
 import org.junit.Test;
 
 public class EnumUtilTest {
 
     @Test
     public void getEnumByCode() {
+        int code = UserTypeEnum.Admin.getCode();
+        assert code == EnumUtil.getEnumByCode(code, UserTypeEnum.values()).getCode();
     }
 
     @Test
     public void contain() {
+        int code = UserTypeEnum.Admin.getCode();
+        assert EnumUtil.contain(code, UserTypeEnum.values());
+        assert !EnumUtil.contain(3, UserTypeEnum.values());
     }
 }
