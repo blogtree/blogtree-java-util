c91ce3995da03613a7b5a06637909370735a8326
AlanGeeker <alangeeker@outlook.com>
2021-10-24 20:49:59 +0800

    BaseEnumTest

--- blogtree-util-api/src/test/java/org/blogtree/util/api/base/BaseEnumTest.java
+++ blogtree-util-api/src/test/java/org/blogtree/util/api/base/BaseEnumTest.java
@@ -1,7 +1,22 @@
 package org.blogtree.util.api.base;
 
-import static org.junit.Assert.*;
+import org.blogtree.util.api.base.enums.UserTypeEnum;
+import org.junit.Test;
 
 public class BaseEnumTest {
+
+    @Test
+    public void isEqualByCode() {
+        int codeAdmin = UserTypeEnum.Admin.getCode();
+        int codeUser = UserTypeEnum.User.getCode();
+        assert UserTypeEnum.Admin.isEqualByCode(codeAdmin);
+        assert !UserTypeEnum.Admin.isEqualByCode(codeUser);
+    }
+
+    @Test
+    public void isEqualByName() {
+        assert UserTypeEnum.Admin.isEqualByName("Admin");
+        assert !UserTypeEnum.Admin.isEqualByName("User");
+    }
 
 }
